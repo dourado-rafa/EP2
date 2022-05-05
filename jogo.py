50c31b0d47bbef497be076af84929fd87971d2cc
Rafael Dourado <rafaeldbo@al.insper.edu.br>
2022-05-04 22:08:11 -0300

    adicionando desistência e inventário

--- jogo.py
+++ jogo.py
@@ -15,12 +15,20 @@ while statusJogo:
     loja = montando_loja(infosPais)
     dicas = {}
 
-    while tentativas > 0 and statusJogando:
+    while (tentativas > 0 and statusJogando):
         jogada = input('Qual seu palpite? ').lower()
 
         if jogada == 'dica':
             dicas, tentativas, loja = menu_dicas(infosPais, loja, tentativas, dicas)
-                
+            exibe_infos(paisesTestados, tentativas, dicas)
+
+        elif jogada == 'desisto':
+            desistir = input("Tem certeza que deseja desistir da rodada? [s|n] ")
+            statusJogando = (desistir == 'n')
+
+        elif jogada == 'inventario':
+            exibe_infos(paisesTestados, tentativas, dicas)
+
         elif jogada in dadosPaises.keys():
             paisTestado = dadosPaises[jogada]
             tentativas -= 1
@@ -28,18 +36,15 @@ while statusJogo:
             if jogada == pais:
                 print('Você venceu')
                 statusJogando = False
-                reiniciar = input('Você quer jogar novamente? ')
-                if reiniciar == 'sim':
-                    statusJogo = True
-                else:
-                    statusJogo = False
+                reiniciar = input('Você quer jogar novamente? [s|n] ')
+                statusJogo = (reiniciar == 's')
 
             else:
                 distancia = haversine(infosPais, paisTestado)
                 paisesTestados = adiciona_em_ordem(jogada, distancia, paisesTestados)
-
+                exibe_infos(paisesTestados, tentativas, dicas)
+        
         else:
             print('País desconhecido')
             
-        exibe_infos(paisesTestados, tentativas, dicas)
                 
