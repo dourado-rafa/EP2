cfdbce292af4dd9aca2611b1bc4acc2b4261e57c
Beatriz Rodrigues <beatrizrf1@al.insper.edu.br>
2022-05-13 15:28:45 -0300

    comentários

--- jogo.py
+++ jogo.py
@@ -2,9 +2,11 @@ from funcoes import *
 from dados import *
 import os
 
+# Começa com uma base de dados por país
 dadosPaises = normaliza(DADOS)
 statusJogo = True
 
+# Início ou reinício do jogo
 os.system('cls')
 print(f""" ============================ 
 |                            |
@@ -13,6 +15,7 @@ print(f""" ============================
  ==== Design de Software ==== """)
 
 while statusJogo: 
+# É o looping do jogo, o statusJogo só passa a ser False se o jogador finalizar a rodada e não quiser jogar novamente.
     print(f"""\n - Comandos:
     dica       - entra no mercado de dicas
     desisto    - desiste da rodada
@@ -21,7 +24,7 @@ while statusJogo:
 Um país foi escolhido, tente adivinhar!\n
 Você tem {cores['ciano']}20{cores['reset']} tentativa(s)""")
 
-    pais = sorteia_pais(dadosPaises)
+    pais = sorteia_pais(dadosPaises) 
     infosPais = dadosPaises[pais]
 
     tentativas = 20
@@ -29,17 +32,28 @@ Você tem {cores['ciano']}20{cores['reset']} tentativa(s)""")
     statusJogando = True
 
     loja = montando_loja(infosPais)
-    dicas = {}
+    # A função loja é utilizada para saber quais dicas o usuário nao pode mais pedir por não ter mais informações disponíveis sobre aquela categoria.
+    # Não analisa o que o usuário pode pedir de acordo com o número de tentativas que ele pode gastar
+
+    dicas = {} 
+    # O dicionário dica guarda as informações já obtidas pelo usuário
+    # São as informações printadas após o usuário pedir uma nova dica
 
     while (tentativas > 0 and statusJogando):
+    # É o looping da rodada, o usuário só sai do looping se perder o jogo por não ter mais tentativas ou desistir da rodada
         jogada = input('Qual seu palpite? ').lower()
 
         if jogada in ['dica', 'dicas']:
             dicas, tentativas, loja = menu_dicas(infosPais, loja, tentativas, dicas)
+        # A função menu_dicas printa as opções de dica que o usuário pode pedir de acordo com as informações disponíveis (loja) e com o número de tentativas.
+        # Após o jogador escolher a dica desejada as tentativas, informações disponíveis e informações obtidas pelo usuário são atualizadas.
             exibe_infos(paisesTestados, tentativas, dicas)
+        # exibe as informações obtidas pelo usuário após a jogada, paises testados e suas distâncias ao país sorteado, 
+        # numero de tentativas e informações obtidas sobre o pais sorteado.
 
         elif jogada == 'desisto':
             statusJogando = desistencia(pais,statusJogando)
+        # a função desistência confirma se o usuário realmente deseja desistir e finaliza a rodada caso sim.
 
         elif jogada == 'inventario':
             exibe_infos(paisesTestados, tentativas, dicas)
